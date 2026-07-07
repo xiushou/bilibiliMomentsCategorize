e91bdb3b41b813a4c42c690ae223df50ea912409
aurxsiu <xiushou71@qq.com>
2026-07-07 11:46:17 +0800

    fix: 无需手动获取cookie

--- extension/content.js
+++ extension/content.js
@@ -1,27 +1,4 @@
-let timer = null;
-
-function checkCookie() {
-  chrome.runtime.sendMessage(
-    {
-      type: "GET_COOKIE",
-    },
-    (res) => {
-      console.log("开始检测 Cookie: " + res.value);
-
-      if (!res?.value) return;
-
-      clearInterval(timer);
-
-      replacePage(res.value);
-    },
-  );
-}
-
-timer = setInterval(checkCookie, 500);
-
-checkCookie();
-
-async function replacePage(cookie) {
+async function replacePage() {
   const html = await fetch(chrome.runtime.getURL("page.html")).then((r) =>
     r.text(),
   );
@@ -30,13 +7,9 @@ async function replacePage(cookie) {
   document.write(html);
   document.close();
 
-  window.__COOKIE__ = cookie;
-
-  document.body.dataset.cookie = cookie;
-
-  console.log("Cookie 已注入到页面: " + window.__COOKIE__);
-
   const script = document.createElement("script");
   script.src = chrome.runtime.getURL("page.js");
   document.body.appendChild(script);
 }
+
+replacePage();
--- extension/page.js
+++ extension/page.js
@@ -1,4 +1,24 @@
-document.getElementById("cookie").innerText =
-  document.body.dataset.cookie || "未检测到 Cookie";
+async function getBilibiliDynamic() {
+  try {
+    const res = await fetch(
+      "https://api.bilibili.com/x/polymer/web-dynamic/v1/feed/all?timezone_offset=-480&type=all&platform=web&offset=1214599979474092040&page=3&features=itemOpusStyle,listOnlyfans,opusBigCover,onlyfansVote,decorationCard,onlyfansAssetsV2,forwardListHidden,ugcDelete,onlyfansQaCard,commentsNewVersion,avatarAutoTheme,sunflowerStyle,cardsEnhance,eva3CardOpus,eva3CardVideo,eva3CardComment,eva3CardVote,eva3CardUser&web_location=333.1365&x-bili-device-req-json=%7B%22platform%22:%22web%22,%22device%22:%22pc%22,%22spmid%22:%22333.1365%22%7D",
+      {
+        method: "GET",
+        credentials: "include",
+      },
+    );
 
-console.log("页面已加载，Cookie: " + document.body.dataset.cookie);
+    if (!res.ok) throw new Error(`HTTP ${res.status}`);
+    const data = await res.json();
+    return data;
+  } catch (e) {
+    console.error("请求异常", e);
+    return null;
+  }
+}
+
+// 调用
+(async () => {
+  const res = await getBilibiliDynamic();
+  console.log("返回数据：", JSON.stringify(res, null, 10));
+})();

a2aa06dc3ff516ee5c7516834d8be23bb7ad9650
aurxsiu <xiushou71@qq.com>
2026-07-08 18:26:16 +0800

    fix: css

--- extension/content.js
+++ extension/content.js
@@ -7,6 +7,11 @@ async function replacePage() {
   document.write(html);
   document.close();
 
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = chrome.runtime.getURL("style.css");
+  document.head.appendChild(link);
+
   const script = document.createElement("script");
   script.src = chrome.runtime.getURL("page.js");
   document.body.appendChild(script);
